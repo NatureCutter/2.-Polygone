304c2432ad4a5cba157ebe38ba4b7fe62446a0bf
Julian Wegner <Julian.wegner1@gmx.de>
2013-04-25 11:34:23 +0200

    2. Polygon

---
+++ application.macosx/source/Polygon.java
@@ -0,0 +1,150 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import controlP5.*; 
+import processing.pdf.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Polygon extends PApplet {
+
+
+
+
+ControlP5 cp5;
+int n;
+float r;
+float hOben;                  // ist gleich hNutz aus den Notizen
+float hUnten;
+int k;
+float Radius = 200;
+int Ecken = 4;
+float HoeheOben = 0;
+float HoeheUnten = 0;
+boolean print = false;
+PFont f;
+
+
+public void setup() {
+
+  
+  size(800, 760 ); 
+  background(235);
+  smooth();
+  
+  f = createFont("helvetica", 14, true);
+  textFont(f,16);                 // STEP 4 Specify font to be used
+  fill(0);                        // STEP 5 Specify font color 
+  text("Press s to save the Polygon",30,110);  // STEP 6 Display Text
+  
+  cp5 = new ControlP5(this);
+  cp5.addSlider("Radius").setPosition(30, 30).setRange(30, 200);
+  cp5.addSlider("Ecken").setPosition(30, 50).setWidth(400).setRange(0, 38).setNumberOfTickMarks(38).setSliderMode(Slider.FIX);
+  cp5.addSlider("HoeheOben").setPosition(30, 70).setRange(0, 200);
+  cp5.addSlider("HoeheUnten").setPosition(30, 90).setRange(0, 200);
+}
+
+public void draw() {
+
+  background(205);
+  
+  n = Ecken;
+  r = Radius;
+  hOben =HoeheOben;
+  hUnten = HoeheUnten; 
+  
+  textFont(f,12);                 // STEP 4 Specify font to be used
+  fill(255);                        // STEP 5 Specify font color 
+  text("Press s to save the Polygon",30,120);  // STEP 6 Display Text
+if (print == true) {
+    background(255);
+  }
+ 
+   pushMatrix();
+   translate(width/2, height/2);
+   
+float g = r * 2*sin(PI/n);                       //Grundseite der Dreiecke im Polygon
+float h = sqrt(sq(r) - sq(g/2));                 //H\u00f6he der Dreiecke im Polygon
+float hNeuOben = sqrt(sq(h) + sq(hOben));        //tats\u00e4chliche H\u00f6he
+float hNeuUnten = sqrt(sq(h) + sq(hUnten));      //tats\u00e4chliche H\u00f6he
+float winkel = atan((g/2) / hNeuOben);                        //alpha in den Notizen
+
+
+for (int i=0 ; i<n; i++) {
+  pushMatrix();
+   rotate(2* winkel * i);
+  
+
+  beginShape();
+
+  vertex(0, 0);                        //1 - Spitze
+  vertex(-(g/2), hNeuOben);            //2 - links
+  vertex(g/2, hNeuOben);               //4 - rechts
+  vertex(-(g/2), hNeuOben);            //2 - links
+
+int l = 30;
+int lUnten; 
+  if (n>6) {
+  lUnten = n ; 
+  }
+  else {
+   lUnten = 0; 
+  }
+  vertex(-(g/2), hNeuOben + l);
+  vertex(-l, hNeuOben + hNeuUnten - lUnten );
+  vertex(0, hNeuOben + hNeuUnten);
+  vertex(-(g/2), hNeuOben);
+
+  vertex(0, hNeuOben + hNeuUnten);     //3 - untere Spitze
+  
+  vertex(l, hNeuOben + hNeuUnten - lUnten);
+  vertex(g/2, hNeuOben + l);
+  vertex(g/2, hNeuOben);
+  vertex(0, hNeuOben + hNeuUnten);
+  
+  vertex(g/2, hNeuOben);               //4 - rechts
+  vertex(0, 0);                        //1 - Spitze
+  
+  endShape();
+  popMatrix();
+}
+stroke(0);
+   
+   
+   
+   popMatrix();
+}
+
+public void keyPressed() {
+  
+  if(key == 's' || key == 'S') {
+  print = true;
+  }
+}
+
+public void keyReleased() {
+    if(key == 's' || key == 'S') {
+
+ saveFrame("Polygon-####.png");  
+  print = false; 
+}}
+
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Polygon" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
